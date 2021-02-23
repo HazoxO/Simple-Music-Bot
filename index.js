60e00978d05849f2bb5be44889f7e06852d99421
HazoxO <73402453+HazoxO@users.noreply.github.com>
2021-02-23 00:09:26 -0600

    Todos los comandos en la index.js

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ bot.status({
 
 }) 
 
+bot.variables({
+ "duration": ""
+}) 
+
 bot.onMessage() 
 
 bot.command({
@@ -68,3 +72,82 @@ $color[RANDOM] `
 
 })
 
+bot.command({
+	name: "np",
+    code: `
+$setUserVar[duration;$sendMessage[{title:$songInfo[title]}{url:$songInfo[url]}{description:$getObjectProperty[bar]
+\`${current1} / ${duration1}\`
+
+Requested by <@$songInfo[userID]>}{thumbnail:$songInfo[thumbnail]}{color:GREEN}{footer:© KasumiChan | Music}{author:Now Playing:$userAvatar[$clientID]};yes]]
+$djseval[const util = require('dbd.js-utils')
+d.object.bar = util.progressBar(${current}, ${duration}, 20, "📀", "▬", "▬")]
+$createObject[{}]`
+})
+
+bot.command({
+  name: "queue",
+  aliases: "playlist", 
+  code: `
+  $onlyIf[$voiceID!=;{color:RED}{description: <:PeligroxX:785381669648597085> Favor Unete A Un Canal De Voz Primero.}]
+  $thumbnail[$serverIcon]
+  $title[Playlist del servidor!]
+  $description[10 primeras canciones de la lista:
+  
+  $queue[1]
+  
+  Canal: <#$voiceID>
+  Pedido por: <@$songInfo[userID]>] 
+  $footer[Requested By $username[$authorID];$authorAvatar]
+  $addTimestamp
+  $color[RANDOM] 
+  `
+})
+
+bot.command({
+  name: "volume", 
+  aliases: "v", 
+  code:`
+  $argsCheck[>1;Sube el volumen de la música a un máximo de \`100\`]
+  $onlyIf[$message[1]<=100;{description:❌  No puedes subir el volumen más de 100!}{color:RANDOM}]
+  $if[$message[1]<=100]
+  $volume[$message[1]]
+  $endIf
+  $if[$message[1]>=101]
+  $addField[No se pudo!;Perdón pero no puedes subir más 100 de volumen!;no]
+  $endIf
+  $color[RANDOM]
+  $description[El volumen subió a **$message[1]/100**]
+  $deletecommand
+  `
+}) 
+
+bot.command({
+  name: "skip", 
+  code: `
+  $onlyIf[$voiceID!=;{color:RED}{description: :ModeBotError: Favor Unete A Un Canal De Voz Primero.}]
+  $description[La cancion ha sido skippeada con éxito!]
+  $color[RANDOM]
+  $skipSong
+  $deletecommand
+  `
+})
+
+bot.command({
+  name: "help",
+  code: `
+$title[Comandos de <@$clientID>]
+$description[**Bienvenido a help!**
+
+\`play\` - comando donde buscas la cancion y la reproduces.
+\`queue\` - puedes ver la lista de reproduccion.
+\`skip\` - cambia la cancion.
+\`stop\`- deten la cancion.
+\`volume\` - sube o baja el volumen de tu canción.
+\`np\` - puedes ver en que minuto esta tu canción.
+
+*Servidor de soporte*
+[dudas o quejas](https://discord.gg/4xCjYzypww)] 
+$footer[Requested By $username[$authorID];$authorAvatar]
+$color[RANDOM]
+`
+})
